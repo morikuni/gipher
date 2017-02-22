a789dc0b86df963ad6498dae748bb614afb698b9
morikuni <taihei.with+git@gmail.com>
2017-02-22 18:56:33 +0900

    Add PasswordCryptor

---
+++ password_cryptor.go
@@ -0,0 +1,62 @@
+package gipher
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"io"
+)
+
+type passwordCryptor struct {
+	passwordHash []byte
+}
+
+func NewPasswordCryptor(password []byte) Cryptor {
+	hash := sha256.Sum256(password)
+	return passwordCryptor{
+		hash[:],
+	}
+}
+
+func (e passwordCryptor) Encrypt(text string) (Base64String, error) {
+	plaintext := []byte(text)
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(e.passwordHash)
+	if err != nil {
+		return "", fmt.Errorf("cannot accept the encryption key: %s", err)
+	}
+	stream := cipher.NewCTR(block, iv)
+
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return Base64String(base64.StdEncoding.EncodeToString(ciphertext)), nil
+}
+
+func (e passwordCryptor) Decrypt(text Base64String) (string, error) {
+	ciphertext, err := base64.StdEncoding.DecodeString(string(text))
+	if err != nil {
+		return "", fmt.Errorf("failed to decode ciphertext as base64: %s", err)
+	}
+	plaintext := make([]byte, len(ciphertext)-aes.BlockSize)
+
+	iv := ciphertext[:aes.BlockSize]
+
+	block, err := aes.NewCipher(e.passwordHash)
+	if err != nil {
+		return "", fmt.Errorf("cannot accept the decryption key: %s", err)
+	}
+	stream := cipher.NewCTR(block, iv)
+
+	stream.XORKeyStream(plaintext, ciphertext[aes.BlockSize:])
+
+	return string(plaintext), nil
+}
